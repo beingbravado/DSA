e54f33c8c5736f5f3723c5f6e3bf11df0dba3278
beingbravado <sharmagyanesh199@gmail.com>
2021-05-20 12:31:07 +0530

    binomial coefficient

--- DP412.java
+++ DP412.java
@@ -5,22 +5,21 @@ public class DP412 {
 
     public static int nCr(int n, int r) {
         // code here
-        long[] fact = new long[n + 1];
-        Arrays.fill(fact, 0);
-        fact[0] = 1;
-        if (r > n)
-            return 0;
-        int num = (int) (((factorial(fact, n) / ((factorial(fact, n - r) * factorial(fact, r) % MOD))) + MOD) % MOD);
-        for (int i = 0; i <= n; i++)
-            System.out.print(fact[i] + " ");
-        System.out.println();
-        return num;
+        long[][] fact = new long[n + 1][r + 1];
+        // Arrays.fill(fact, 0);
+        long num = C(n, r, fact) % MOD;
+        return (int)num;
     }
 
-    public static long factorial(long[] fact, int n) {
-        if (fact[n] > 0)
-            return fact[n];
-        return fact[n] = factorial(fact, n - 1) * n % MOD;
+    public static long C(int n, int r, long[][] fact) {
+        if (r > n)
+            return 0;
+        if (r == 0 || r == n)
+            return fact[n][r] = 1;
+
+        if (fact[n][r] >0)
+            return fact[n][r] % MOD;
+        return fact[n][r] = C(n - 1, r - 1, fact) % MOD + C(n - 1, r, fact) % MOD;
     }
 
     public static void main(String[] args) throws Exception {
